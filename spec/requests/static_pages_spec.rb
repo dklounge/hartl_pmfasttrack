263dded7fb6f432bfdcf47e1d1b3efed32777d26
David Kim <daviddarden11@gmail.com>
2014-01-16 22:46:51 -0800

    bunch of layout updates'

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,45 +2,46 @@ require 'spec_helper'
 
 describe "Static pages" do
 
+  subject { page }
+
   describe "Home page" do
-    it "should have the content 'Sample App'" do
-      visit '/static_pages/home'
-      page.should have_content('Sample App')
-    end
+    before { visit root_path }
 
-    it "should have bsae title'" do
-      visit '/static_pages/home'
-      page.should have_title('Sample App')
-    end
-
-    it "should not have custom page title" do
-      visit '/static_pages/home'
-      page.should_not have_title('| Home')
-    end
+    it { should have_content('Please Refactor') }
+    it { should have_title("Refactor") }
+    it { should_not have_title('| Home') }
   end
 
   describe "Help page" do
-    it "should have the content 'Help'" do
-      visit '/static_pages/help'
-      page.should have_content('Help')
-    end
+    before { visit help_path }
 
-    it "should have the title 'Help'" do
-      visit '/static_pages/help'
-      page.should have_title('Sample App | Help')
-    end
+    it { should have_content('Help') }
+    it { should have_title("Refactor | Help") }
   end
 
   describe "About page" do
-    it "should have the content 'About Us'" do
-      visit '/static_pages/about'
-      page.should have_content('About Us')
+
+    it "should have the h1 'About Us'" do
+      visit about_path
+      expect(page).to have_content('About Us')
     end
 
     it "should have the title 'About Us'" do
-      visit '/static_pages/about'
-      page.should have_title('Sample App | About Us')
+      visit about_path
+      expect(page).to have_title("Refactor | About Us")
     end
   end
 
+  describe "Contact page" do
+
+    it "should have the content 'Contact'" do
+      visit contact_path
+      expect(page).to have_content('Contact')
+    end
+
+    it "should have the title 'Contact'" do
+      visit contact_path
+      expect(page).to have_title("Refactor | Contact")
+    end
+  end
 end
